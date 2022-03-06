10ffd01a2843186dd84dc21b64e7c92d9e8a6fcb
topcueser <topcueser@gmail.com>
2022-03-07 01:38:12 +0300

    MobilePhoneFactory private constructor

--- src/com/esertopcu/dpatterns/factory/MobilePhoneFactory.java
+++ src/com/esertopcu/dpatterns/factory/MobilePhoneFactory.java
@@ -2,11 +2,16 @@ package com.esertopcu.dpatterns.factory;
 
 public class MobilePhoneFactory {
 
+    private MobilePhoneFactory() {
+
+    }
+
     public static Mobile getMobilePhone(MobilePhoneType type, String name, double price) {
         return switch (type) {
             case APPLE   -> new Apple(name, price);
             case SAMSUNG -> new Samsung(name, price);
             case XIAOMI  -> new Xiaomi(name, price);
+            default -> throw new RuntimeException("Invalid Mobile Type!");
         };
     }
 }
